57d996fe7e0b7389b5227ac7a4e4a456a74c92e0
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-09-05 21:37:07 +0600

    Add Module 03 - Creating a Simple Web Page -> Lesson 04 - Django and the Flow of Control

--- django_getting_started/urls.py
+++ django_getting_started/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from website.views import welcome
+from website.views import welcome, date
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('welcome.html', welcome),
+    path('', welcome),
+    path('date', date)
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 
 
 def welcome(request):
     return HttpResponse("Welcome to the meeting planner")
+
+
+def date(request):
+    return HttpResponse("This page was served at: " + str(datetime.now()))
