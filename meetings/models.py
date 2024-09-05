cfe3a9b2e518549b4c3c6446691b4ca9ae07c207
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-09-06 02:46:27 +0600

    Add Module 04 - Setting up a Data Model -> Lesson 07 - Bringing It All Together

--- meetings/models.py
+++ meetings/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from datetime import time
 
 
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"Meeting: {self.title}, for {self.duration} hr(s), on {self.date} starts at: {self.start_time}"
