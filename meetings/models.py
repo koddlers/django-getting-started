27adbd3e3c32e4e2049d38f181c30907a98f68f2
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-09-06 03:06:54 +0600

    Add Module 04 - Setting up a Data Model -> Lesson 08 - Adding a Second Model Class

--- meetings/models.py
+++ meetings/models.py
@@ -2,11 +2,21 @@ from django.db import models
 from datetime import time
 
 
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    floor = models.IntegerField()
+    room_number = models.IntegerField()
+
+    def __str__(self):
+        return f"Room: {self.name} - Floor: {self.floor} - Room No: {self.room_number}"
+
+
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Meeting: {self.title}, for {self.duration} hr(s), on {self.date} starts at: {self.start_time}"
